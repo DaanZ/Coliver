5eafd7861822e888ac123177aad377602406815d
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-06 16:20:51 +0700

    added info

--- classify.py
+++ classify.py
@@ -9,12 +9,18 @@ from pydantic import Field, BaseModel
 from chatgpt import llm_strict
 from history import History
 
-categories: list[str] = ["Sauna instructions", "Parking my vehicle", "Kiosk instructions", "None of the above"]
+categories: list[str] = ["Sauna instructions", "Parking my vehicle", "Kiosk instructions", "Groceries / Fridge instructions", "Washing Machines", "Activities Materials", "First Aid Supplies", "None of the above"]
 
 images = {"Sauna instructions": [{"path": os.path.join(rootpath.detect(), "images", "sauna_knobs.png"), "caption": "Sauna Knobs"}],
           "Parking my vehicle": [{"path": os.path.join(rootpath.detect(), "images", "parking_1.jpg"), "caption": "Parking Entrance"},
                                  {"path": os.path.join(rootpath.detect(), "images", "parking_2.jpg"), "caption": "Don't park in front of the houses."}],
-          "Kiosk instructions": [{"path": os.path.join(rootpath.detect(), "images", "kiosk.jpg"), "caption": "Electrical socket for Kiosk"}]}
+          "Kiosk instructions": [{"path": os.path.join(rootpath.detect(), "images", "kiosk.jpg"), "caption": "Electrical socket for Kiosk"}],
+          "Groceries / Fridge instructions": [{"path": os.path.join(rootpath.detect(), "images", "fridge.jpg"), "caption": "Use your own label to put your groceries in the fridge"},
+                                 {"path": os.path.join(rootpath.detect(), "images", "cupboard.jpg"), "caption": "Or use the cupboards with your our label."}],
+          "First Aid Supplies": [{"path": os.path.join(rootpath.detect(), "images", "first_aid.jpg"), "caption": "First aid kit located on top of the Laundry room"}],
+          "Washing Machines": [{"path": os.path.join(rootpath.detect(), "images", "washing_machines.jpg"), "caption": "Washing Machines located outside please use your own detergent and your own products please"}],
+          "Activities Materials": [{"path": os.path.join(rootpath.detect(), "images", "activities.jpg"), "caption": "You can borrow the activities in exchange for good care."}]
+          }
 
 
 def classify_conversation(user: str):
